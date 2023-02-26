1ecd327938f797409d5e01f26ce6bb432812c8ff
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2023-02-26 07:12:30 -0800

    checking error

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-"""
-This module contains endpoint(route) status
-"""
-from models import storage
-from flask import Flask
+""" API test stats"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status')
 def status():
-    """
-    Returns a JSON status
-    """
+    """status"""
     return jsonify({"status": "OK"})
 
-@app_views.route('/stats', strict_slashes=False)
-def count():
-    """
-    Retrieves the number of each objects by type
-    """
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
 
+@app_views.route('/stats')
+def stats():
+    """stats"""
+    return jsonify({
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+        })
